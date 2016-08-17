2b7ce9ce13b568008ac3f1d6a23066fc308cdd67
Dat Nguyen <nguyen.dat@bcgdv.com>
2016-08-17 11:37:54 -0700

    update server.js

--- server.js
+++ server.js
@@ -5,20 +5,12 @@ var builder = require('botbuilder');
 // Bot Setup
 //=========================================================
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.verifyBotFramework(), connector.listen());
-
 
 //=========================================================
 // Bots Dialogs
@@ -28,11 +20,18 @@ bot.dialog('/', function (session) {
     session.send("Hello World");
 });
 
+// Setup Restify Server
+var server = restify.createServer();
+
+// Handle Bot Framework messages
+server.post('/api/messages', connector.listen());
+
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-    'directory': '.',
-    'default': 'index.html'
+	'directory': '.',
+	'default': 'index.html'
 }));
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
